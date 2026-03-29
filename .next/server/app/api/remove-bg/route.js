f7f9d15d5bc628dd0aa4f031fbbfeecef283c1f8
root <root@localhost.localdomain>
2026-03-29 23:29:00 +0800

    fix: use npm install instead of npm ci

---
+++ .next/server/app/api/remove-bg/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/remove-bg/route.js")
+R.c("server/chunks/[root-of-the-server]__04b.o9w._.js")
+R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
+R.c("server/chunks/_next-internal_server_app_api_remove-bg_route_actions_09_9l2i.js")
+R.m(48606)
+module.exports=R.m(48606).exports
